037160c9035c21fc2f559b8a51f39f22e36bca05
Alex Olivas <alex.r.olivas@gmail.com>
2024-04-15 16:37:40 -0600

    small fixes to naming.

--- tests/test_device_manager.cpp
+++ tests/test_device_manager.cpp
@@ -1,19 +1,17 @@
 //
 // Created by olivas on 4/15/24.
 //
-#include <cassert>
 
 #include <iostream>
 #include <memory>
-
 #include <cudapp/device_manager.hpp>
 
 using std::cerr;
 using std::endl;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char** argv){
 
-    std::shared_ptr <DeviceManager> device(new DeviceManager);
+    std::shared_ptr <DeviceManager> device_mgr(new DeviceManager);
 }
 
 
